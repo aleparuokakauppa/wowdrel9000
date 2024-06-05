44e115cc5adf863f37833504b3b8e7c8eda5452a
aleparuokakauppa <aleksileinonen2@gmail.com>
2024-06-05 21:01:50 +0300

    b

--- main.go
+++ main.go
@@ -2,12 +2,34 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"math/rand"
 	"net/http"
 	"os"
 )
+
+var answer string
+
+type Guess struct {
+    Version int `json:"version"`
+    Guess string `json:"guess"`
+}
+
+type Letter struct {
+    Char rune `json:"char"`
+    Match bool `json:"match"`
+    Close bool `json:"close"`
+    Miss bool `json:"miss"`
+}
+
+type GuessResponse struct {
+    Version int `json:"version"`
+    Win bool `json:"win"`
+    Letters [5]Letter `json:"letters"`
+}
 
 func servePage(w http.ResponseWriter, r *http.Request) {
     http.ServeFile(w, r, "web/index.html")
@@ -39,11 +61,72 @@ func gameServerHandler() {
     if err != nil {
         log.Fatal(err)
     }
-    randWord := words[rand.Intn(len(words))]
+    answer = words[rand.Intn(len(words))]
+}
+
+func compareGuess(guess Guess) [5]Letter {
+    letters := new([5]Letter)
+    for _, letter := range letters {
+        letter.Miss = true;
+    }
+
+    for guessIndex, guessRune := range guess.Guess {
+        for answerIndex, answerRune := range answer {
+            if guessRune == answerRune {
+                if guessIndex == answerIndex {
+                    letters[guessIndex].Match = true;
+                    letters[guessIndex].Miss = false;
+                }
+                letters[guessIndex].Close = true;
+                letters[guessIndex].Miss = false;
+            }
+        }
+    }
+    return *letters
+}
+
+func handleGuess(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        http.Error(w, "Invalid request method", http.StatusBadRequest)
+        return
+    }
+
+    body, err := io.ReadAll(r.Body)
+    if err != nil {
+        http.Error(w, "Unable to read request body", http.StatusBadRequest)
+        return
+    }
+    defer r.Body.Close()
+
+    var guess Guess
+    err = json.Unmarshal(body, &guess) 
+    if err != nil {
+        http.Error(w, "Unable to parse JSON data", http.StatusBadRequest)
+        return
+    }
+
+    if guess.Version != 1 {
+        http.Error(w, "Wrong protocol version. Want version 1.", http.StatusBadRequest)
+    }
+
+    letters := compareGuess(guess)
+
+    response := GuessResponse{
+        Version: 1,
+        Win: true,
+    }
+
+    for _, letter := range letters {
+        if letter.Miss == true {
+            response.Win = false
+        }
+
+    }
 }
 
 func main() {
     http.HandleFunc("/", servePage)
+    http.HandleFunc("/guess", handleGuess)
 
     fs := http.FileServer(http.Dir("web"))
     http.Handle("/static/", http.StripPrefix("/static/", fs))
